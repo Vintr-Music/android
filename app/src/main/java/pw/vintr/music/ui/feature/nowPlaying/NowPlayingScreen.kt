5a9995b0363c4e6d08ccc5594c3ad6a1d0d675bb
Ruslan Barcho <vinterruslan@gmail.com>
2023-12-12 05:21:08 +0300

    VMAND-Player: Flexible controls

--- app/src/main/java/pw/vintr/music/ui/feature/nowPlaying/NowPlayingScreen.kt
+++ app/src/main/java/pw/vintr/music/ui/feature/nowPlaying/NowPlayingScreen.kt
@@ -2,23 +2,16 @@ package pw.vintr.music.ui.feature.nowPlaying
 
 import android.graphics.Bitmap
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -27,9 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -40,16 +31,11 @@ import androidx.palette.graphics.Palette
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import org.koin.androidx.compose.getViewModel
-import pw.vintr.music.R
-import pw.vintr.music.domain.player.model.config.PlayerRepeatMode
-import pw.vintr.music.domain.player.model.config.PlayerShuffleMode
 import pw.vintr.music.domain.player.model.state.PlayerStatusModel
 import pw.vintr.music.tools.extension.Dash
 import pw.vintr.music.tools.extension.Space
-import pw.vintr.music.ui.kit.button.ButtonPlayerState
-import pw.vintr.music.ui.kit.button.SimpleIconButton
+import pw.vintr.music.ui.kit.player.PlayerControls
 import pw.vintr.music.ui.kit.player.PlayerProgressBar
-import pw.vintr.music.ui.theme.Bee0
 import pw.vintr.music.ui.theme.Gilroy16
 import pw.vintr.music.ui.theme.Gilroy24
 import pw.vintr.music.ui.theme.VintrMusicExtendedTheme
@@ -157,81 +143,24 @@ fun NowPlayingScreen(viewModel: NowPlayingViewModel = getViewModel()) {
             onSeek = { viewModel.onSeek(it) },
             onSeekEnd = { viewModel.onSeekEnd() }
         )
-        Box(
-            modifier = Modifier
-                .padding(20.dp)
-                .navigationBarsPadding()
-                .clip(RoundedCornerShape(20.dp))
-                .background(MaterialTheme.colorScheme.background.copy(alpha = 0.9f))
-                .border(
-                    1.dp,
-                    VintrMusicExtendedTheme.colors.playerSliderStroke,
-                    RoundedCornerShape(20.dp)
-                )
-                .padding(20.dp)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                SimpleIconButton(
-                    iconRes = when (playerState.value.repeatMode) {
-                        PlayerRepeatMode.OFF,
-                        PlayerRepeatMode.ON_SESSION -> R.drawable.ic_repeat
-                        PlayerRepeatMode.ON_SINGLE -> R.drawable.ic_repeat_single
-                    },
-                    size = 40.dp,
-                    iconModifier = Modifier.size(28.dp),
-                    tint = when (playerState.value.repeatMode) {
-                        PlayerRepeatMode.OFF -> VintrMusicExtendedTheme.colors.textRegular
-                        PlayerRepeatMode.ON_SESSION,
-                        PlayerRepeatMode.ON_SINGLE -> Bee0
-                    },
-                    onClick = { viewModel.setNextRepeatMode(playerState.value.repeatMode) },
-                )
-                Spacer(modifier = Modifier.width(20.dp))
-                SimpleIconButton(
-                    iconRes = R.drawable.ic_skip_backward,
-                    size = 40.dp,
-                    iconModifier = Modifier.size(28.dp),
-                    onClick = { viewModel.backward() }
-                )
-                Spacer(modifier = Modifier.width(20.dp))
-                ButtonPlayerState(
-                    isPlaying = playerState.value.status == PlayerStatusModel.PLAYING,
-                    onClick = {
-                        when (playerState.value.status) {
-                            PlayerStatusModel.IDLE,
-                            PlayerStatusModel.LOADING,
-                            PlayerStatusModel.PAUSED -> {
-                                viewModel.resume()
-                            }
-                            PlayerStatusModel.PLAYING -> {
-                                viewModel.pause()
-                            }
-                        }
+        PlayerControls(
+            playerState = playerState.value,
+            onBackward = { viewModel.backward() },
+            onChangePlayerState = {
+                when (playerState.value.status) {
+                    PlayerStatusModel.IDLE,
+                    PlayerStatusModel.LOADING,
+                    PlayerStatusModel.PAUSED -> {
+                        viewModel.resume()
                     }
-                )
-                Spacer(modifier = Modifier.width(20.dp))
-                SimpleIconButton(
-                    iconRes = R.drawable.ic_skip_forward,
-                    size = 40.dp,
-                    iconModifier = Modifier.size(28.dp),
-                    onClick = { viewModel.forward() }
-                )
-                Spacer(modifier = Modifier.width(20.dp))
-                SimpleIconButton(
-                    iconRes = R.drawable.ic_shuffle,
-                    size = 40.dp,
-                    iconModifier = Modifier.size(28.dp),
-                    tint = when (playerState.value.shuffleMode) {
-                        PlayerShuffleMode.OFF -> VintrMusicExtendedTheme.colors.textRegular
-                        PlayerShuffleMode.ON -> Bee0
-                    },
-                    onClick = { viewModel.setNextShuffleMode(playerState.value.shuffleMode) },
-                )
-            }
-        }
+                    PlayerStatusModel.PLAYING -> {
+                        viewModel.pause()
+                    }
+                }
+            },
+            onForward = { viewModel.forward() },
+            onSetNextRepeatMode = { viewModel.setNextRepeatMode(playerState.value.repeatMode) },
+            onSetNextShuffleMode = { viewModel.setNextShuffleMode(playerState.value.shuffleMode) }
+        )
     }
 }
---
+++ app/src/main/java/pw/vintr/music/ui/kit/player/PlayerControls.kt
@@ -0,0 +1,112 @@
+package pw.vintr.music.ui.kit.player
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pw.vintr.music.R
+import pw.vintr.music.domain.player.model.config.PlayerRepeatMode
+import pw.vintr.music.domain.player.model.config.PlayerShuffleMode
+import pw.vintr.music.domain.player.model.state.PlayerStateHolderModel
+import pw.vintr.music.domain.player.model.state.PlayerStatusModel
+import pw.vintr.music.ui.kit.button.ButtonPlayerState
+import pw.vintr.music.ui.kit.button.SimpleIconButton
+import pw.vintr.music.ui.theme.Bee0
+import pw.vintr.music.ui.theme.VintrMusicExtendedTheme
+
+@Composable
+@Preview(widthDp = 400)
+fun PlayerControls(
+    playerState: PlayerStateHolderModel = PlayerStateHolderModel(),
+    onBackward: () -> Unit = {},
+    onChangePlayerState: () -> Unit = {},
+    onForward: () -> Unit = {},
+    onSetNextRepeatMode: (PlayerRepeatMode) -> Unit = {},
+    onSetNextShuffleMode: (PlayerShuffleMode) -> Unit = {},
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .padding(20.dp)
+            .navigationBarsPadding()
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(20.dp))
+            .background(MaterialTheme.colorScheme.background.copy(alpha = 0.9f))
+            .border(
+                1.dp,
+                VintrMusicExtendedTheme.colors.playerSliderStroke,
+                RoundedCornerShape(20.dp)
+            )
+            .padding(vertical = 20.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        val width = with (LocalDensity.current) {
+            constraints.maxWidth
+                .toDp()
+                .coerceAtMost(340.dp)
+        }
+
+        Row(
+            modifier = Modifier
+                .width(width),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            SimpleIconButton(
+                iconRes = when (playerState.repeatMode) {
+                    PlayerRepeatMode.OFF,
+                    PlayerRepeatMode.ON_SESSION -> R.drawable.ic_repeat
+                    PlayerRepeatMode.ON_SINGLE -> R.drawable.ic_repeat_single
+                },
+                size = 40.dp,
+                iconModifier = Modifier.size(28.dp),
+                tint = when (playerState.repeatMode) {
+                    PlayerRepeatMode.OFF -> VintrMusicExtendedTheme.colors.textRegular
+                    PlayerRepeatMode.ON_SESSION,
+                    PlayerRepeatMode.ON_SINGLE -> Bee0
+                },
+                onClick = { onSetNextRepeatMode(playerState.repeatMode) },
+            )
+            SimpleIconButton(
+                iconRes = R.drawable.ic_skip_backward,
+                size = 40.dp,
+                iconModifier = Modifier.size(28.dp),
+                onClick = { onBackward() }
+            )
+            ButtonPlayerState(
+                isPlaying = playerState.status == PlayerStatusModel.PLAYING,
+                onClick = { onChangePlayerState() }
+            )
+            SimpleIconButton(
+                iconRes = R.drawable.ic_skip_forward,
+                size = 40.dp,
+                iconModifier = Modifier.size(28.dp),
+                onClick = { onForward() }
+            )
+            SimpleIconButton(
+                iconRes = R.drawable.ic_shuffle,
+                size = 40.dp,
+                iconModifier = Modifier.size(28.dp),
+                tint = when (playerState.shuffleMode) {
+                    PlayerShuffleMode.OFF -> VintrMusicExtendedTheme.colors.textRegular
+                    PlayerShuffleMode.ON -> Bee0
+                },
+                onClick = { onSetNextShuffleMode(playerState.shuffleMode) },
+            )
+        }
+    }
+}
