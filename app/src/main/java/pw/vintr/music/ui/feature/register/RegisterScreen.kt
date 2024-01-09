c5a7c1a6ce23f92639377960f1bb39a7ff8e1a80
Ruslan Barcho <vinterruslan@gmail.com>
2024-01-09 06:08:39 +0300

    VMAND-Register: Register screen + VM

--- app/src/main/java/pw/vintr/music/ui/feature/register/RegisterScreen.kt
+++ app/src/main/java/pw/vintr/music/ui/feature/register/RegisterScreen.kt
@@ -1,17 +1,32 @@
 package pw.vintr.music.ui.feature.register
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
 import org.koin.androidx.compose.getViewModel
 import pw.vintr.music.R
 import pw.vintr.music.tools.composable.StatusBarEffect
+import pw.vintr.music.ui.kit.button.ButtonRegular
+import pw.vintr.music.ui.kit.input.AppTextField
 import pw.vintr.music.ui.kit.toolbar.ToolbarRegular
 
 @Composable
@@ -19,6 +34,8 @@ fun RegisterScreen(
     viewModel: RegisterViewModel = getViewModel()
 ) {
     StatusBarEffect()
+
+    val screenState = viewModel.screenState.collectAsState()
 
     Scaffold(
         modifier = Modifier
@@ -30,11 +47,65 @@ fun RegisterScreen(
                 onBackPressed = { viewModel.navigateBack() }
             )
         },
-    ) {
+    ) { scaffoldPadding ->
         Column(
-            modifier = Modifier.padding(it)
+            modifier = Modifier
+                .padding(scaffoldPadding)
+                .fillMaxSize()
+                .imePadding()
         ) {
-
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState()),
+                verticalArrangement = Arrangement.spacedBy(20.dp)
+            ) {
+                AppTextField(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    label = stringResource(id = R.string.email),
+                    hint = stringResource(id = R.string.email),
+                    value = screenState.value.email,
+                    onValueChange = { viewModel.changeEmail(it) },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Email,
+                        imeAction = ImeAction.Next,
+                    )
+                )
+                AppTextField(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    label = stringResource(id = R.string.enter_password),
+                    hint = stringResource(id = R.string.enter_password),
+                    value = screenState.value.password,
+                    visualTransformation = PasswordVisualTransformation(),
+                    onValueChange = { viewModel.changePassword(it) },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done,
+                    )
+                )
+                AppTextField(
+                    modifier = Modifier.padding(horizontal = 20.dp),
+                    label = stringResource(id = R.string.repeat_password),
+                    hint = stringResource(id = R.string.repeat_password),
+                    value = screenState.value.password,
+                    visualTransformation = PasswordVisualTransformation(),
+                    onValueChange = { viewModel.changeRepeatPassword(it) },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Password,
+                        imeAction = ImeAction.Done,
+                    )
+                )
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            ButtonRegular(
+                modifier = Modifier.padding(horizontal = 20.dp),
+                text = stringResource(id = R.string.login),
+                enabled = screenState.value.formIsValid,
+                isLoading = screenState.value.isRegistering,
+                onClick = { viewModel.register() },
+            )
+            Spacer(modifier = Modifier.height(32.dp))
         }
     }
 }
--- app/src/main/java/pw/vintr/music/ui/feature/register/RegisterViewModel.kt
+++ app/src/main/java/pw/vintr/music/ui/feature/register/RegisterViewModel.kt
@@ -1,5 +1,39 @@
 package pw.vintr.music.ui.feature.register
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import pw.vintr.music.tools.extension.Empty
 import pw.vintr.music.ui.base.BaseViewModel
 
-class RegisterViewModel : BaseViewModel()
+class RegisterViewModel : BaseViewModel() {
+
+    private val _screenState = MutableStateFlow(RegisterScreenState())
+
+    val screenState = _screenState.asStateFlow()
+
+    fun changeEmail(value: String) {
+        _screenState.update { it.copy(email = value) }
+    }
+
+    fun changePassword(value: String) {
+        _screenState.update { it.copy(password = value) }
+    }
+
+    fun changeRepeatPassword(value: String) {
+        _screenState.update { it.copy(repeatPassword = value) }
+    }
+
+    fun register() {
+        // TODO: register
+    }
+}
+
+data class RegisterScreenState(
+    val email: String = String.Empty,
+    val password: String = String.Empty,
+    val repeatPassword: String = String.Empty,
+    val isRegistering: Boolean = false,
+) {
+    val formIsValid = email.isNotBlank() && password.isNotBlank() && password == repeatPassword
+}
